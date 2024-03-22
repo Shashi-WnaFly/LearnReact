705dd3b9178d553fa4fce3cbba67205753a4f324
Shashi Anand <shashianand2600@gmail.com>
2024-03-22 23:01:59 +0530

    First day of react

---
+++ Inception/App.js
@@ -0,0 +1,4 @@
+let heading = React.createElement("h1", {id:"heading", babu:"sona"}, "Hello World From React!");
+let root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(heading);
