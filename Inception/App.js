2e2971ca70342bb2dd5a18bfe044fc427090385b
Shashi Anand <shashianand2600@gmail.com>
2024-03-23 12:40:20 +0530

    Day 2 of learing react

--- Inception/App.js
+++ Inception/App.js
@@ -1,4 +1,19 @@
-let heading = React.createElement("h1", {id:"heading", babu:"sona"}, "Hello World From React!");
-let root = ReactDOM.createRoot(document.getElementById('root'));
+let heading = React.createElement("h1", { id: "heading", babu: "sona" }, "Hello World From React!");
+let root = ReactDOM.createRoot(document.getElementById("root"));
+let body = ReactDOM.createRoot(document.querySelector("body"));
 
-root.render(heading);
+// root.render(heading);
+
+let heading1 = React.createElement("div", { id: "parent" },
+    [React.createElement("div", { id: "child" },
+        [React.createElement("h1", {}, "Namaste React."),
+        React.createElement("h2", {}, "I'm the h2 tag.")]
+    ),
+
+    React.createElement("div", { id: "child2" },
+        [React.createElement("h1", {}, "Namaste React."),
+        React.createElement("h2", {}, "I'm the h2 tag.")]
+    )]
+);
+
+root.render(heading1);
