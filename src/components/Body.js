e8aa687beaee9239e96486c88db0acde1d7a3ebf
Shashi Anand <shashianand2600@gmail.com>
2024-04-18 22:53:50 +0530

    learn about React hook useState

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,25 @@
 import Restaurant from "../utils/mockedData"
 import RestaurantCard from "./RestaurantCard";
+// import topRatedRestaurant from "./topRatedRestaurant";
+import { useState } from "react";
 
-const Body = ()=>(
-    <div className="body">
-        <div className="res-container">
-            {
-                Restaurant.map( (restaurant) => (<RestaurantCard key={restaurant.info.id} resData={restaurant}/>))
-            }
+const Body = ()=>{
+    let [RestaurantList, setRestaurant] = useState(Restaurant);
+
+    return (
+        <div className="body">
+            <div>
+                <button className="top-btn" onClick={()=>{
+                    setRestaurant(RestaurantList.filter( (obj) => obj.info.avgRating > 4));
+                }}>Top Rated Restaurant</button>
+            </div>
+            <div className="res-container">
+                {
+                    RestaurantList.map( (RestaurantList) => (<RestaurantCard key={RestaurantList.info.id} resData={RestaurantList}/>))
+                }
+            </div>
         </div>
-    </div>
-);
+    )
+}
 
 export default Body;
---
+++ src/components/topRatedRestaurant.js
@@ -0,0 +1,11 @@
+// import { useState } from "react";
+// import Restaurant from "../utils/mockedData";
+
+// let [RestaurantList, setRestaurantList] = useState(Restaurant);
+// function topRatedRestaurant(){
+//     let filteredList = RestaurantList.filter( (obj)=> obj.info.avgRating >= 4 );
+//     setRestaurantList(filteredList);
+//     console.log(RestaurantList);
+// }
+
+// export default topRatedRestaurant;
