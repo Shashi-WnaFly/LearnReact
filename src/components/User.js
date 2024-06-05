22f5653e3048982b9f87f0305ab51dd49d43f6fa
Shashi Anand <shashianand2600@gmail.com>
2024-06-05 17:00:30 +0530

    learn how to create state var in class based component

--- src/components/User.js
+++ src/components/User.js
@@ -1,10 +1,18 @@
+import {useState} from "react";
+
 const User = (props) => {
     const {name, location} = props;
+    const [ count, setCount ] = useState(0);
+    // const [ count1 ] = useState(1)
     return (
         <div style={{border:"2px solid black", padding:"1rem"}}>
             <p>Name : {name}</p>
             <p>Location : {location}</p>
-            <p>Job : null</p>
+            <h2>count : {count}</h2>
+            <button onClick={()=>{
+                setCount(count+1)
+            }}>Count Increaser</button>
+            {/* <h2>count1 : {count1}</h2> */}
         </div>
     )
 }
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -2,19 +2,28 @@ import React from "react";
 
 class UserClass extends React.Component {
 
-  Constructor(props) {
-    // super(props); // here super key generating an error and whole project not render but in the
-    // video it works fine.
+  constructor(props) {
+    super(props);
+    this.state = {
+      count : 0,
+      count1 : 1
+    }
   }
-
+  
   render() {
-    // const { name, location } = this.props;
     const {name, location} = this.props;
+    const { count, count1 } = this.state;
     return (
       <div style={{ border: "2px solid salmon", padding: "1rem" }}>
         <p>Name : {name}</p>
         <p>Location : {location}</p>
-        <p>Job : null</p>
+        <h2>count : {count}</h2>
+        <button onClick={()=>{
+          this.setState({
+            count: this.state.count+1,
+          })
+        }}>Count Increase</button>
+        <h2>count1 : {count1}</h2>
       </div>
     );
   }
